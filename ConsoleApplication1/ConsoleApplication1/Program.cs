4d021de332f5c11daf3e3840af2a3e6fc9e8d148
saidev <saidevm@hotmail.com>
2017-11-19 08:15:08 +0000

    ch1 vs

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             //foo create
-            //foo update in git web
+            //foo update in git1 vs
         }
     }
 }
