80852a38de967a490434a3b2a4d00110216f80b1
saidevm <saidevm@vs2012>
2017-11-25 07:41:29 +0000

    mvc routes

---
+++ MVCWebApplicationFoo/MVCWebApplicationFoo/App_Start/RouteConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MVCWebApplicationFoo
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            // Example to map non default route
+            routes.MapRoute(
+                name: "Serial Number",
+                url: "serial/{lcase}",
+                defaults: new
+                {
+                    controller = "Home",
+                    action = "Serial",
+                    lcase = "upper"
+                }
+            );
+
+            // Default route
+            routes.MapRoute(
+                name: "Default", 
+                url: "{Controller}/{Action}", 
+                defaults: new {
+                    Controller = "Home",
+                    Action = "Index"
+                });
+        }
+    }
+}
